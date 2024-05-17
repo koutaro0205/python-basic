42b82c17405f0e054123bfa7219a64b8b25e7321
koutaro0205 <koutaro130205@icloud.com>
2024-05-17 11:18:43 +0900

    feat

--- price_down.py
+++ price_down.py
@@ -6,7 +6,7 @@ first_arg = args[1]
 second_arg = args[2]
 
 #品目（品名と価格）を辞書型で定義
-hinmoku = {
+ITEM_PRICE = {
   "リンゴ": 80,
   "みかん": 198,
   "バナナ": 150,
@@ -17,23 +17,23 @@ hinmoku = {
   "パスタ": 258
 }
 
-fruits = ("リンゴ", "みかん", "バナナ")
-alcohol = ("ビール", "日本酒")
-noodles = ("ラーメン", "うどん", "パスタ")
+FRUITS = ("リンゴ", "みかん", "バナナ")
+ALCOHOL = ("ビール", "日本酒")
+NOODLES = ("ラーメン", "うどん", "パスタ")
 
 #ここ以降にプログラムを書く
 
 TYPE_MAP = {
-  "麺類": noodles,
-  "酒類": alcohol,
-  "果物類": fruits,
+  "麺類": NOODLES,
+  "酒類": ALCOHOL,
+  "果物類": FRUITS,
 }
 
-def price_down(type, price):
-  array = TYPE_MAP[type]
-  for key in array:
-    prevPrice = hinmoku[key]
-    hinmoku[key] = prevPrice - price if prevPrice - price > 0 else 1
-  print(hinmoku)
+def price_down(type, price_reduction_amount):
+  items = TYPE_MAP[type]
+  for key in items:
+    prevPrice = ITEM_PRICE[key]
+    ITEM_PRICE[key] = prevPrice - price_reduction_amount if prevPrice - price_reduction_amount > 0 else 1
+  print(ITEM_PRICE)
 
 price_down(first_arg, int(second_arg))
